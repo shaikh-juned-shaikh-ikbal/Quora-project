d2842fc7c0a65cbf95fd846458254fe94d99c8f7
sachin <sksachinkaushik5362@gmail.com>
2023-07-24 00:21:18 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -46,16 +46,7 @@ app.get("/", (req, res) => {
   });
 
 
- app.use("/uploads", express.static(path.join(__dirname, "/../uploads")));
-app.use(express.static(path.join(__dirname, "/../frontend/build")));
 
-app.get("*", (req, res) => {
-    try {
-      res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
-    } catch (e) {
-      res.send("Oops! unexpected error");
-    }
-  });
   
 
 
